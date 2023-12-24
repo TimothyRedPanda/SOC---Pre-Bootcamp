e48ee45e9868891d29b64e43e35afeeea081f490
TimothyRedPanda <151554578+TimothyRedPanda@users.noreply.github.com>
2023-12-24 13:46:29 +0000

    Added animation keyframes for icons and hid the scrollbar on the place icons containter - it also now scrolls horizontally using the mouse wheel - or drag on touch screen

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const nav = gsap.utils.toArray(".icons2")
+const nav = document.querySelectorAll(".icons2")
 nav.forEach((nav) => {
 	nav.addEventListener("click", () => {
 		document.querySelector(".active")?.classList.remove("active")
@@ -6,10 +6,17 @@ nav.forEach((nav) => {
 	})
 })
 
-const span = gsap.utils.toArray(".Heart")
+const span = document.querySelectorAll(".Heart")
 span.forEach((span) => {
 	span.addEventListener("click", () => {
 		document.querySelector(".active2")?.classList.remove("active2")
 		span.classList.add("active2")
 	})
 })
+
+let scrollContainer = document.querySelector('.icons-filter-places');
+
+scrollContainer.addEventListener('wheel', (evt) => {
+	evt.preventDefault();
+	scrollContainer.scrollLeft += evt.deltaY;
+});
